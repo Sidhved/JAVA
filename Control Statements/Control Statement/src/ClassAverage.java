c5cb2ca37be2f6aba1e5bf36a276f4ea63175423
Sidhved <sidhved.warik@gmail.com>
2021-05-25 00:28:30 +0530

    Initialised Control Statements Directory

---
+++ Control Statements/Control Statement/src/ClassAverage.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ClassAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //initialization phase
+        int total = 0;
+        int gradeCounter = 1;
+
+        //processing phase uses counter controlled repetition
+        while (gradeCounter <= 10) {
+            System.out.print("Enter grade: ");
+            int grade = sc.nextInt();
+            total += grade;
+            gradeCounter++;
+        }
+
+        //termination phase
+        int avg = total/10;
+
+        //display total and avg grades
+        System.out.printf("%nTotal of all 10 grades is %d%n", total);
+        System.out.printf("Class Average is %d%n", avg);
+    }
+}
