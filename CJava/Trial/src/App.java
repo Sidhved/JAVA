08c974f126777d94b2ac71b8fcdbab3f59f9032f
Sidhved <sidhved.warik@gmail.com>
2021-06-05 21:17:29 +0530

    Initialised CJava

---
+++ CJava/Trial/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int x = 7;
+        int y = x;
+        x = 2;
+        System.out.println(x +", "+y);
+    }
+}
